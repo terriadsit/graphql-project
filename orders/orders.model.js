d917f3d3c5d97aebca29d031a630a2ccfee8599f
terriadsit <terriadsit@yahoo.com>
2022-10-19 16:47:43 -0700

    server schema and initial models created

---
+++ orders/orders.model.js
@@ -0,0 +1,21 @@
+// placeholders for data model
+module.exports = [
+  {
+    orders: [
+      {
+        date: '2005-05-06',
+        subtotal: 90.22,
+        items: [
+          {
+            product: {
+              id: 'redshoe',
+              description: 'Old Red Shoe',
+              price: 45.11
+            },
+            quantity: 2
+          }
+        ]
+      }
+    ]
+  }
+];
---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql'); // an express middleware function
+
+const { loadFilesSync } = require('@graphql-tools/schema');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+
+const schema = makeExecutableSchema({
+    typeDefs: typesArray
+})
+
+const root = {
+    products: require('./products/products.model'),
+    orders: require('./orders/orders.model')
+}
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
+app.listen(3000, () => {
+    console.log('running graphql server...');
+});
+
