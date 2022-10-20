eb0863000c3012a7d617c8d280192b75ea5a9e2c
terriadsit <terriadsit@yahoo.com>
2022-10-20 11:03:42 -0700

    add mutations

--- products/products.model.js
+++ products/products.model.js
@@ -3,12 +3,14 @@ const products = [
   {
     id: 'redshoe',
     description: 'Red Shoe',
-    price: 42.12
+    price: 42.12,
+    reviews: [],
   },
   {
     id: 'bluejean',
     description: 'Blue Jeans',
-    price: 55.55
+    price: 55.55,
+    reviews: [],
   }
 ]
 
@@ -16,6 +18,51 @@ function getAllProducts() {
   return products;
 }
 
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  })
+} 
+
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  })
+}
+
+function addNewProduct(id, description, price ) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const newReview = {
+    rating,
+    comment,
+  };
+  console.log('in model', newReview)
+  const matchedProduct = getProductById(id)
+  console.log('in model', matchedProduct)
+  if (matchedProduct) {
+    matchedProduct.reviews.push(newReview);
+    return newReview;
+  } else {
+    return ('error, invalid product id')
+  }
+  
+  
+}
+
 module.exports = {
   getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+  addNewProductReview,
 }
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -4,6 +4,21 @@ module.exports = {
     Query: {
         products: () => {
             return productsModel.getAllProducts();
+        },
+        productsByPrice: (_, args) => {
+            return productsModel.getProductsByPrice(args.min, args.max);
+        },
+        product: (_, args) => {
+            return productsModel.getProductById(args.id);
         }
+    },
+    Mutation: {
+        addNewProduct: (_, args) => {
+            return productsModel.addNewProduct(args.id, args.description, args.price);
+        },
+        addNewProductReview: (_, args) => {
+            console.log('in resolver, args', args.id, args.rating, args.comment)
+             return productsModel.addNewProductReview(args.id, args.rating, args.comment);
+        },
     }
 };
